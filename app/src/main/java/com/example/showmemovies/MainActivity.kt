0a348c34d06d626c38abd6c6e845e384562b5b13
Anand Parshuramka <anand.parshuramka@flipkart.com>
2024-02-23 08:47:18 +0530

    feat(movies): adding movies module

--- app/src/main/java/com/example/showmemovies/MainActivity.kt
+++ app/src/main/java/com/example/showmemovies/MainActivity.kt
@@ -3,17 +3,9 @@ package com.example.showmemovies
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -25,29 +17,18 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-            ) {
-                MyApp()
-            }
+            MyApp()
         }
     }
 }
 
-@Preview(showBackground = true, name = "Text preview")
-@Composable
-fun Profile() {
-    Box(modifier = Modifier.fillMaxWidth().fillMaxHeight().background(Color.Red)){
-        Text( text = "Profile")
-    }
-}
 
 
 // Define the MyApp composable, including the `NavController` and `NavHost`.
 @Composable
 fun MyApp() {
     val navController = rememberNavController()
-    NavHost(navController, startDestination = "profile") {
-        composable("profile") { Profile() }
+    NavHost(navController, startDestination = "movies") {
+        composable("movies") { MovieHomePage() }
     }
 }
--- movies/src/main/java/com/example/showmemovies/MovieHomePage.kt
+++ movies/src/main/java/com/example/showmemovies/MovieHomePage.kt
@@ -1,19 +1,68 @@
 package com.example.showmemovies
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview(showBackground = true, name = "Text preview")
 @Composable
-fun Profile() {
-    Box(modifier = Modifier.fillMaxWidth().fillMaxHeight().background(Color.Red)){
-        Text( text = "Profile")
+fun MovieHomePage() {
+    HomeScreen()
+}
+
+
+@Composable
+fun HomeScreen() {
+    // this is the most outer box that will
+    // contain all the views,buttons,chips,etc.
+    Box(
+        modifier = Modifier
+            .background(Color.Blue)
+            .fillMaxSize()
+    ) {
+        Column {
+            // this is how we call
+            // function adding whole UI
+            GreetingSection()
+        }
+    }
+}
+
+@Composable
+fun GreetingSection(
+    name: String = "Geeks"
+) {
+    // here we just arrange the views
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            // heading text view
+            Text(
+                text = "Good morning, $name", style = MaterialTheme.typography.h1
+            )
+            Text(
+                text = "We wish you have a good day!", style = MaterialTheme.typography.body1
+            )
+        }
     }
 }
